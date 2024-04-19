50ff67b467e75fb216f260ab3fd7611f6c69079a
Ronan <rozer223@gmail.com>
2024-04-19 16:55:03 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Hello World! Welcome to Nuxt 3 and Capacitor!</h1>
+        <button @click="share">Example share button!</button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { Share } from '@capacitor/share';
+
+async function share() {
+    await Share.share({
+        title: 'Open Youtube',
+        text: 'Check new video on youtube',
+        url: 'https://www.youtube.com',
+        dialogTitle: 'Share with friends'
+    });
+}
+</script>
